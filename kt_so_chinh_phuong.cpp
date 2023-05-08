4dc1430e287f6a785b9edb4e01fa0673e33647a9
lekimanh452003 <101647674+lekimanh452003@users.noreply.github.com>
2023-05-08 12:21:14 +0700

    Add files via upload

---
+++ kt_so_chinh_phuong.cpp
@@ -0,0 +1,25 @@
+#include <iostream.h>
+#include <math.h>
+bool kt_SCP(int n)//la so nguyen duong co can bac 2 la 1 so nguyen
+{
+	int a=sqrt(n);
+	if(a*a==n)
+	{
+		return true;
+	}else{
+		return false;
+	}
+}
+int main()
+{
+	int n;
+	cout<<"\nn= ";cin>>n;
+	kt_SCP(n);
+	if(kt_SCP(n)==true)
+	{
+		
+	}else{
+		cout<<"\nko phai so chinh phuong"<<endl;
+	}
+	return 0;
+}
